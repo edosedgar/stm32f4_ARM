87a5a267954ceb07ca635f600c082fc96617c5bc
Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>
2018-02-14 03:42:02 +0300

    smt32f4: Simple clock
    Signed-off-by: Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>

--- TemplateProject/src/main.c
+++ TemplateProject/src/main.c
@@ -5,14 +5,44 @@
 
 void SystemClock_Config(void);
 
+void
+delay(uint32_t cycle) {
+        while (cycle--);
+}
+
+uint8_t digit[4] = {0, 0, 0, 0};
+uint8_t codes[11] = {0b01111011, 0b01000001, 0b10111001, 0b11101001,
+                     0b11000011, 0b11101010, 0b11111010, 0b01100001,
+                     0b11111011, 0b11101011, 0b00000100};
 int
 main(void) {
-        SystemClock_Config();
-        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
-        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
-        LL_GPIO_SetOutputPin(GPIOD, LL_GPIO_PIN_13);
+        uint8_t dig_num = 0;
 
-        while (1);
+        SystemClock_Config();
+        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);
+        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
+
+        while (1) {
+                LL_GPIO_WriteOutputPort(GPIOD, digit[dig_num]);
+                LL_GPIO_WriteOutputPort(GPIOC, ~(1 << dig_num));
+                dig_num++;
+                dig_num %= 4;
+                delay(10000);
+                LL_GPIO_WriteOutputPort(GPIOC, ~0);
+        }
 }
 
 /**
@@ -62,4 +92,118 @@ SystemClock_Config() {
         /* Update CMSIS variable (which can be updated also
          * through SystemCoreClockUpdate function) */
         SystemCoreClock = 168000000;
+}
+
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+int tick = 0;
+int sec = 0;
+int min = 44;
+int hours = 10;
+void SysTick_Handler(void)
+{
+        tick++;
+        if (tick == 1000) {
+                sec++;
+                if (sec == 60) {
+                        min++;
+                        if (min == 60) {
+                                hours++;
+                                hours %= 24;
+                                min = 0;
+                        }
+                        sec = 0;
+                }
+                digit[3] = codes[min%10];
+                digit[2] = codes[min/10];
+                digit[1] = codes[hours%10] | (codes[10] * (sec % 2));
+                digit[0] = codes[hours/10];
+                tick = 0;
+        }
 }
